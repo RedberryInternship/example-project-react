09763c39abf7f3e54b303fc5f8f4e210238c11fb
beqramo <beqa31198@gmail.com>
2019-12-24 13:41:42 +0400

    style:changed component

--- src/hooks/rootRun.ts
+++ src/hooks/rootRun.ts
@@ -1,5 +1,4 @@
-import React, {useEffect, useState,useRef, useCallback} from "react";
-import {AppState, Keyboard, Alert, } from "react-native"
+import React, {useEffect, useState,useRef} from "react";
 import { useAppState } from 'react-native-hooks';
 import {useNetInfo} from "@react-native-community/netinfo";
 import AsyncStorage, {useAsyncStorage} from "@react-native-community/async-storage";
