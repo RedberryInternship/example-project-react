e635107ff49f4b8cadd58178c124858895ddb294
beqramo <bekakokhodze98@gmail.com>
2020-04-21 17:37:12 +0400

    feat:updating started with failure android version updated successfully ios has some problems, there is a solutions on internet react-hook-form has proxy deasn't exist error on this version, workeround: add polifil proxxy by hand, but it doesn't seems right to me, waiting new releases

--- android/app/src/main/java/com/boxwood/espaceboxwoodrelease/MainApplication.java
+++ android/app/src/main/java/com/boxwood/espaceboxwoodrelease/MainApplication.java
@@ -2,7 +2,7 @@ package com.boxwood.espaceboxwoodrelease;
 
 import android.app.Application;
 import android.content.Context;
-
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 
@@ -54,7 +54,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    // initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
@@ -62,15 +62,17 @@ public class MainApplication extends Application implements ReactApplication {
    *
    * @param context
    */
-  private static void initializeFlipper(Context context) {
+  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     if (BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+        Class<?> aClass = Class.forName("com.boxwood.espaceboxwoodrelease.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
