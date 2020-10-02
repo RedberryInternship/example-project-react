661bafc112b62da72fe4ea77c052b1dfaeb3f808
giunashvili <giorgi.official@yahoo.com>
2020-10-02 17:35:34 +0400

    add: enums.ts

---
+++ src/utils/enums.ts
@@ -0,0 +1,21 @@
+export enum ChargerFilters {
+  FREE,
+  CHARGING,
+  FAST,
+  LVL2,
+  PUBLIC,
+  PRIVATE,
+}
+
+export enum ChargerStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  CHARGING = 'CHARGING',
+  NOT_PRESENT = 'NOT_PRESENT',
+}
+
+export enum ConnectorTypes {
+  TYPE_2 = 'Type 2',
+  COMBO_2 = 'Combo 2',
+  CHADEMO = 'CHAdeMO',
+}
