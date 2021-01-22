c33a341684e68c8cc1bf64645eb73fdcc74b01b3
giunashvili <giorgi.official@yahoo.com>
2021-01-22 16:59:59 +0400

    fix: change map mode

--- src/screens/drawer/setting/settings/useSettings.ts
+++ src/screens/drawer/setting/settings/useSettings.ts
@@ -58,7 +58,7 @@ export default (navigation: Navigation) => {
       setUserDataInStorage()
       dispatch(editUserInfo(newSelectedItem, UserSettingEnum.mapMode))
     }
-  }, [selectedItem])
+  }, [selectedItem, dispatch, t])
 
   /**
    * Set user data on local state, whenever
