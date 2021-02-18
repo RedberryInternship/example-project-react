9fa133b802dc8f12d57cce24c0840384f77806a1
giunashvili <giorgi.official@yahoo.com>
2021-02-18 11:07:42 +0400

    chore: working on transactions

--- src/screens/drawer/transactions/showTransaction/ShowTransaction.tsx
+++ src/screens/drawer/transactions/showTransaction/ShowTransaction.tsx
@@ -21,18 +21,18 @@ const ShowTransactions: FCWithNavigation = ({ navigation }) => {
   const { t } = useTranslation()
   const { params } = useRoute<any>()
   const {
-    charger_name,
-    address,
-    duration,
-    penalty_fee,
-    charge_power,
-    start_date,
-    charge_price,
     user_card_pan,
+    charger_name,
+    charge_power,
+    charge_price,
+    penalty_fee,
+    start_date,
+    duration,
+    address,
   }: TransactionsHistoryResponseItem = params.order
 
   const penaltyFee = (): string => `${penalty_fee ?? 0} ${t('gel')}`
-  const chargePrice = (): string => `${charge_price ?? 0} ${t('gel')}`
+  const chargePrice = (): string => `${charge_price ?? 0} ₾`
   const durationInMins = (): string => `${duration ?? 0} ${t('minute')}`
   const shouldNotRender = (): boolean => !duration && !penalty_fee && !charge_price
 
@@ -41,23 +41,19 @@ const ShowTransactions: FCWithNavigation = ({ navigation }) => {
       <BaseHeader title="transactions.transactions" onPressLeft={navigation.goBack} />
       <View style={styles.innerContainer}>
         <View style={styles.headerContainer}>
-          <Image source={images.transaction} style={styles.transactionIcon} />
-          <BaseText style={styles.title}>
-            {' '}
-            {charger_name}
-            {' '}
-          </BaseText>
-          <BaseText style={styles.dateAndTime}>
-            {' '}
-            {start_date}
-            {' '}
-          </BaseText>
+          <Image source={images.creditCard} style={styles.transactionIcon} resizeMode="contain" />
+          <View style={styles.titleDateTimeContainer}>
+            <BaseText style={styles.title}>
+              {charger_name}
+            </BaseText>
+            <BaseText style={styles.dateAndTime}>
+              {start_date}
+            </BaseText>
+          </View>
           <BaseText style={styles.price}>{chargePrice()}</BaseText>
         </View>
         <BaseText style={styles.detailsCopy}>
-          {' '}
           {t('transactions.details')}
-          {' '}
         </BaseText>
         {!shouldNotRender() && (
           <View style={styles.detailsContainer}>
@@ -80,9 +76,7 @@ const ShowTransactions: FCWithNavigation = ({ navigation }) => {
         </View>
         <View style={styles.cardDetailsContainer}>
           <BaseText style={styles.cardNumberCopy}>
-            {' '}
             {t('transactions.cardNumber')}
-            {' '}
           </BaseText>
           <BaseText style={styles.cardNumber}>{user_card_pan ?? ''}</BaseText>
         </View>
@@ -101,23 +95,28 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     flex: 1,
-    backgroundColor: Colors.secondaryLightGrey,
+    backgroundColor: Colors.secondaryGray,
     borderRadius: 10,
     margin: 16,
   },
   headerContainer: {
-    backgroundColor: Colors.secondaryGrey,
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.primaryLightGrey,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: Colors.secondaryGrey,
     borderRadius: 10,
     paddingHorizontal: 16,
     paddingBottom: 16,
-    paddingTop: 32,
+    paddingTop: 18,
+
   },
   transactionIcon: {
-    width: 46,
-    height: 33,
+    marginTop: 10,
+    height: 36,
+  },
+  titleDateTimeContainer: {
+    display: 'flex',
   },
   title: {
     fontSize: 15,
@@ -133,20 +132,26 @@ const styles = StyleSheet.create({
   },
   price: {
     marginTop: 16,
-    fontSize: 17,
-    letterSpacing: 0.2,
-    color: Colors.secondaryBlue,
+    fontSize: 36,
+    letterSpacing: 0,
+    color: Colors.primaryGold,
+    fontFamily: 'FiraGO-Book',
+    fontWeight: 'bold',
   },
   detailsCopy: {
-    color: Colors.primaryBackground,
-    marginVertical: 16,
-    marginLeft: 32,
+    color: Colors.primaryWhite,
+    backgroundColor: Colors.primaryBlue,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    marginTop: 40,
+    marginBottom: 16,
+    paddingTop: 15,
+    paddingBottom: 10,
+    paddingLeft: 32,
     fontSize: 15,
     letterSpacing: 0.2,
   },
   detailsContainer: {
-    borderTopWidth: 1,
-    borderTopColor: Colors.primaryLightGrey,
     borderBottomWidth: 1,
     borderBottomColor: Colors.primaryLightGrey,
     paddingHorizontal: 32,
