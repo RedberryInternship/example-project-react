7fc83f0950721cd13d94d66b4af70203a0cdb6cf
giunashvili <giorgi.official@yahoo.com>
2021-04-24 20:14:34 +0400

    add: detox test for incorrect auth try

--- e2e/auth.e2e.ts
+++ e2e/auth.e2e.ts
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {
   element,
-  device,
   waitFor,
-  expect,
+  device,
   by,
 } from 'detox'
 
@@ -15,7 +14,7 @@ beforeEach(async () => {
       location: 'always',
       notifications: 'YES',
     },
-  })
+  });
 });
 
 it('Authenticates without any problems', async () => {
@@ -26,4 +25,13 @@ it('Authenticates without any problems', async () => {
   await element(by.id('AuthButton')).tap();
   await waitFor(element(by.id('TermsAndConditions'))).toBeVisible().withTimeout(3000);
   await element(by.id('AcceptTermsButton')).tap();
+});
+
+it('Has error alerts when not typing into the inputs', async () => {
+  await waitFor(element(by.id('AuthButton'))).toBeVisible().withTimeout(5000);
+  await element(by.id('AuthButton')).tap();
+  await element(by.id('AuthButton')).tap();
+  await waitFor(element(by.text('გთხოვთ ჩაწეროთ ტელეფონის ნომერი')))
+    .toBeVisible()
+    .withTimeout(2000);
 });
