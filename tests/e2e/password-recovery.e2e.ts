976d331b91bb2ae5bfcc7ed395f83f07ba922951
giunashvili <giorgi.official@yahoo.com>
2021-04-29 17:08:44 +0400

    chore: e2e password recovery flow with correct error messages

--- tests/e2e/password-recovery.e2e.ts
+++ tests/e2e/password-recovery.e2e.ts
@@ -5,13 +5,13 @@ import {
   expect,
   by,
 } from 'detox'
-import { fetchUserOTP, resetUserPassword } from './helpers'
+import { fetchUserOTP, resetUserPassword, delay } from './helpers'
 
 const testUserPhone = '500102031';
 const testUserForgottenPassword = 'detoxify';
 const testUserNewPassword = 'capitan-marvel';
 
-beforeAll(async () => {
+beforeEach(async () => {
   await device.launchApp({
     delete: true,
     permissions: {
@@ -22,16 +22,78 @@ beforeAll(async () => {
 });
 
 test('Forgot password functionality is - ok', async () => {
+  /**
+   * Log into app.
+   */
   await waitFor(element(by.id('AuthButton'))).toBeVisible().withTimeout(5000);
   await element(by.id('AuthButton')).tap();
   await element(by.id('ForgotPasswordButton')).tap();
 
+  /**
+   * Type phone number and get one time password.
+   */
   await element(by.id('PhoneInput')).typeText(testUserPhone);
   await element(by.id('SendOTP')).tap();
   const userOTP = await fetchUserOTP(testUserPhone);
   await element(by.id('OneTimePasswordInput')).typeText(userOTP);
   await element(by.id('ForgotPasswordNextButton')).tap();
 
+  /**
+   * Type new password and recover.
+   */
+  await element(by.id('PasswordInput')).typeText(testUserNewPassword);
+  await element(by.id('RepeatPasswordInput')).typeText(testUserNewPassword);
+  await element(by.id('ForgotPasswordNextButton')).tap();
+  await expect(element(by.label('პაროლი წარმატებით შეიცვალა'))).toBeVisible();
+  await resetUserPassword(testUserPhone, testUserForgottenPassword);
+});
+
+test('Forgot password flow has correct error messages', async () => {
+  /**
+   * Log into app.
+   */
+  await waitFor(element(by.id('AuthButton'))).toBeVisible().withTimeout(5000);
+  await element(by.id('AuthButton')).tap();
+  await element(by.id('ForgotPasswordButton')).tap();
+
+  /**
+  * Assert phone number error message.
+  */
+  await element(by.id('ForgotPasswordNextButton')).tap();
+  await waitFor(element(by.text('გთხოვთ ჩაწეროთ ტელეფონის ნომერი')))
+    .toBeVisible()
+    .withTimeout(1000);
+
+  /**
+   * Assert otp error message.
+   */
+  await delay(1);
+  await element(by.id('PhoneInput')).typeText(testUserPhone);
+  await element(by.id('ForgotPasswordNextButton')).tap();
+  await waitFor(element(by.text('შეიყვანეთ sms კოდი')))
+    .toBeVisible()
+    .withTimeout(1000);
+
+  /**
+   * Get one time password and go to next screen.
+   */
+  await element(by.id('SendOTP')).tap();
+  const userOTP = await fetchUserOTP(testUserPhone);
+  await element(by.id('OneTimePasswordInput')).typeText(userOTP);
+  await element(by.id('ForgotPasswordNextButton')).tap();
+
+  /**
+  * Assert password error message.
+  */
+  await delay(1);
+  await element(by.id('ForgotPasswordNextButton')).tap()
+  await waitFor(element(by.text('გთხოვთ, შეავსოთ პაროლის ველები')))
+    .toBeVisible()
+    .withTimeout(1000);
+
+  /**
+   * Type new password and recover.
+   */
   await element(by.id('PasswordInput')).typeText(testUserNewPassword);
   await element(by.id('RepeatPasswordInput')).typeText(testUserNewPassword);
   await element(by.id('ForgotPasswordNextButton')).tap();
